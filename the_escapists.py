e8f2f87991abad6b808793558369cd906b463aa8
EpocDotFr <contact@epoc.fr>
2017-07-26 13:31:58 +0200

    It works!

--- the_escapists.py
+++ the_escapists.py
@@ -1,11 +1,11 @@
 from collections import OrderedDict
 from configparser import ConfigParser
 from mss.windows import MSS as mss
-from ctypes import windll, byref, create_string_buffer
 from ctypes.wintypes import RECT, DWORD
 from hashlib import md5
 from PIL import Image
 from time import sleep
+import ctypes
 import os
 
 
@@ -67,6 +67,59 @@ class ItemsDataParser:
         return craft
 
 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                ("mi", MouseInput),
+                ("hi", HardwareInput)]
+
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
 class ItemsImagesExtractor:
     current_weapon_addr = 0x0E2F4D2C
     weapon_slot_top = 369
@@ -91,17 +144,17 @@ class ItemsImagesExtractor:
 
     def _set_window(self):
         """Set the game's window."""
-        self.game_window = windll.user32.FindWindowW(None, 'The Escapists')
+        self.game_window = ctypes.windll.user32.FindWindowW(None, 'The Escapists')
 
         if not self.game_window:
             raise Exception('The game does not seems to be running')
 
-        windll.user32.SetForegroundWindow(self.game_window)
+        ctypes.windll.user32.SetForegroundWindow(self.game_window)
 
     def _set_weapon_slot_pos(self):
         """Set the weapon slot position in the game's window."""
         game_window_rect = RECT()
-        windll.user32.GetWindowRect(self.game_window, byref(game_window_rect))
+        ctypes.windll.user32.GetWindowRect(self.game_window, ctypes.byref(game_window_rect))
 
         self.weapon_slot_pos = {
             'top': game_window_rect.top + self.weapon_slot_top,
@@ -112,12 +165,12 @@ class ItemsImagesExtractor:
 
     def _set_process(self):
         game_window_proc_id = DWORD()
-        windll.user32.GetWindowThreadProcessId(self.game_window, byref(game_window_proc_id))
+        ctypes.windll.user32.GetWindowThreadProcessId(self.game_window, ctypes.byref(game_window_proc_id))
 
         if not game_window_proc_id:
             raise Exception('Unable to get the game\'s process ID')
 
-        self.game_process = windll.kernel32.OpenProcess(self.PROCESS_ALL_ACCESS, False, game_window_proc_id)
+        self.game_process = ctypes.windll.kernel32.OpenProcess(self.PROCESS_ALL_ACCESS, False, game_window_proc_id)
 
         if not self.game_process:
             raise Exception('Unable open the game\'s process')
@@ -125,9 +178,15 @@ class ItemsImagesExtractor:
     def _set_current_weapon(self, item_id):
         item_id = bytes(item_id, 'utf8')
         buffer_size = len(item_id)
-        current_weapon_id = create_string_buffer(item_id, size=buffer_size)
+        current_weapon_id = ctypes.create_string_buffer(item_id, size=buffer_size)
 
-        windll.kernel32.WriteProcessMemory(self.game_process, self.current_weapon_addr, current_weapon_id, buffer_size)
+        ctypes.windll.kernel32.WriteProcessMemory(self.game_process, self.current_weapon_addr, current_weapon_id, buffer_size)
+
+    def _toggle_profile(self):
+        PressKey(0x08)
+        sleep(0.2)
+        ReleaseKey(0x08)
+        sleep(0.2)
 
     def extract(self):
         scsh = mss()
@@ -137,12 +196,10 @@ class ItemsImagesExtractor:
             # Change the current player's weapon (by writing its ID in the game's memory)
             self._set_current_weapon(item_id)
 
-            sleep(0.5)
+            sleep(0.2)
 
             # Show the player's profile by sending key 7 (required in order to be taken into account by the game)
-            # TODO
-
-            sleep(0.5)
+            self._toggle_profile()
 
             # Take screenshot of the weapon slot
             weapon_slot = scsh.grab(self.weapon_slot_pos)
@@ -162,6 +219,4 @@ class ItemsImagesExtractor:
             weapon_slot_img.save(os.path.join(self.output_dir, item_id + '.png'))
 
             # Hide the player's profile by sending key 7 (required in order to be taken into account by the game)
-            # TODO
-
-            sleep(0.5)
+            self._toggle_profile()
