68d0144ed70bb5fa16050d75a8bd76bc22003800
Epoc <contact@epoc.fr>
2017-08-13 13:45:20 +0200

    I give up sorting items by their name

--- helpers.py
+++ helpers.py
@@ -22,7 +22,7 @@ def load_json(file):
     with open(file, 'r') as f:
         data = f.read()
 
-    return json.loads(data, object_pairs_hook=OrderedDict) if data else data # FIXME object_pairs_hook doesn't seem to work
+    return json.loads(data) if data else data
 
 
 def save_json(file, data):
