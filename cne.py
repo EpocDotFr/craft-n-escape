85581a66feab1de173a3585d13c599e50ca912ef
EpocDotFr <contact@epoc.fr>
2017-10-23 13:19:15 +0200

    Add cache buster to every static assets

--- cne.py
+++ cne.py
@@ -275,3 +275,30 @@ def get_images(game_version=1):
         items_images[detected_image[0]] = detected_image[1]
 
     return items_images
+
+
+# -----------------------------------------------------------
+# Hooks
+
+
+@app.url_defaults
+def hashed_static_file(endpoint, values):
+    """Add a cache-buster value in the URL of each static assets."""
+    if endpoint == 'static':
+        filename = values.get('filename')
+
+        if filename:
+            blueprint = request.blueprint
+
+            if '.' in endpoint:
+                blueprint = endpoint.rsplit('.', 1)[0]
+
+            static_folder = app.static_folder
+
+            if blueprint and app.blueprints[blueprint].static_folder:
+                static_folder = app.blueprints[blueprint].static_folder
+
+            fp = os.path.join(static_folder, filename)
+
+            if os.path.exists(fp):
+                values[int(os.stat(fp).st_mtime)] = ''
