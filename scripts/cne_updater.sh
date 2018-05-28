1b1841c0718d8cdf7b2068c65fb6d1447a1cc210
EpocDotFr <contact@epoc.fr>
2018-05-28 13:17:47 +0200

    Improve updater

--- scripts/cne_updater.sh
+++ scripts/cne_updater.sh
@@ -6,19 +6,23 @@
 
 set -e # Makes any subsequent failing commands to exit the script immediately
 
-echo "## Initializing"
+TYPE=${1:-fast}
+
+echo "## Initializing ($TYPE update)"
 
 . venv/bin/activate
 export FLASK_APP=cne.py
 
-echo "## Updating dependencies"
-
-pip install --upgrade --no-cache -r requirements.txt
-pip install --upgrade --no-cache uwsgi
-
 echo "## Pulling latest code version"
 
 git pull
+
+if [ $TYPE = "full" ]; then
+    echo "## Updating dependencies"
+
+    pip install --upgrade --no-cache -r requirements.txt
+    pip install --upgrade --no-cache uwsgi
+fi
 
 echo "## Restarting services"
 
