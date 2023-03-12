72bc835f6332886b1caed94fcdc1e570a8607435
EpocDotFr <contact@epoc.fr>
2023-03-12 13:47:51 +0100

    Minify HTML responses

--- cne.py
+++ cne.py
@@ -1,68 +1,89 @@
-try:
-    from flask_debugtoolbar import DebugToolbarExtension
-
-    has_debug_toolbar_ext = True
-except ImportError:
-    has_debug_toolbar_ext = False
-
-from logging.handlers import RotatingFileHandler
 from flask_assets import Environment, Bundle
 from flask_caching import Cache
 from environs import Env
 from flask import Flask
-import logging
-
 
 # -----------------------------------------------------------
-# Boot
-
+# App bootstrap
 
 env = Env()
 env.read_env()
 
-
 app = Flask(__name__, static_url_path='')
 
 app.config.update(
+    # Default config values that may be overwritten by environment values
     SECRET_KEY=env.str('SECRET_KEY'),
-    CACHE_TYPE='FileSystemCache',
-    CACHE_DIR='instance/cache',
+
+    CACHE_TYPE=env.str('CACHE_TYPE', default='FileSystemCache'),
+    CACHE_DIR=env.str('CACHE_DIR', default='instance/cache'),
+
+    ASSETS_CACHE=env.str('ASSETS_CACHE', default='instance/webassets-cache'),
+
+    COMPRESS_REGISTER=env.bool('COMPRESS_REGISTER', default=False),
+    MINIFY_HTML=env.bool('MINIFY_HTML', default=False),
+
+    # Config values that cannot be overwritten
     ITEMS_IMAGES_DIR='static/images/items/{game_version}',
     ITEMS_FILE='data/{game_version}/items.json',
     RECIPES_FILE='data/{game_version}/recipes.json',
     ESCAPISTS_WIKI_DOMAIN='theescapists.gamepedia.com',
-    COMPRESS_MIN_SIZE=1024,
 )
 
-cache = Cache(app)
-assets = Environment(app)
+# -----------------------------------------------------------
+# Debugging-related behaviours
 
-if not app.config['DEBUG']:
+if app.config['DEBUG']:
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG)
+
+# -----------------------------------------------------------
+# Flask extensions initialization and configuration
+
+# Flask-DebugToolbar
+if app.config['DEBUG']:
+    try:
+        from flask_debugtoolbar import DebugToolbarExtension
+
+        debug_toolbar = DebugToolbarExtension(app)
+    except ImportError:
+        pass
+
+# Flask-Compress
+try:
     from flask_compress import Compress
 
-    Compress(app)
+    compress = Compress(app)
 
-if has_debug_toolbar_ext:
-    toolbar = DebugToolbarExtension(app)
+    app.config.update(
+        COMPRESS_MIN_SIZE=1024
+    )
+except ImportError:
+    pass
 
-assets.cache = 'instance/webassets-cache/'
+# Flask-HTMLMIN
+try:
+    from flask_htmlmin import HTMLMIN
+
+    htmlmin = HTMLMIN(app)
+except ImportError:
+    pass
+
+# Flask-Assets
+assets = Environment(app)
 
 assets.register('js_home', Bundle('js/common.js', 'js/home.js', filters='jsmin', output='js/home.min.js'))
 assets.register('js_recipes_editor', Bundle('js/common.js', 'js/recipes_editor.js', filters='jsmin', output='js/recipes_editor.min.js'))
 assets.register('css_app', Bundle('css/app.css', filters='cssutils', output='css/app.min.css'))
 
-handler = RotatingFileHandler('instance/logs/errors.log', maxBytes=10000000, backupCount=2)
-handler.setLevel(logging.WARNING)
-formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
-handler.setFormatter(formatter)
-app.logger.addHandler(handler)
+# Flask-Caching
+cache = Cache(app)
 
 from helpers import *
 
-
 # -----------------------------------------------------------
-# After-init imports
-
+# After-bootstrap imports
 
 import routes
 import commands
